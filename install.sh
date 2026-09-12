b29d55b798a95d1d56a3416ddbe8fbf8e7804065
xaxaaxaaa-hash <xaxaaxaaa@gmail.com>
2026-09-12 18:43:59 +0500

    Update install.sh
    Свежий скрипт
    -переделана установка Aurora, запускает автоустановщик с нужного зеркала, выбираем 1-2 и русик
    -скачивание signbox-EX без привязки к версии, качается сразу свежий релиз с нужной архитектурой
    
    СКРИПТ НЕ ПРОВЕРЕН

--- install.sh
+++ install.sh
@@ -1,34 +1,42 @@
-# Обновление базовых пакетов (быстрое)+русификация
+#!/bin/sh
+
 apk update && apk upgrade && apk add luci-i18n-base-ru || {
     echo "Ошибка установки базовых пакетов. WAN подключен? Ебало провайдера представили?"
     exit 1
 }
 
 # Добавление GitHub в hosts (обход блокировок)
-git="github.com"; grep -q "^140.82.114.3 $git" /etc/hosts || { 
-    printf "#$git\n140.82.114.3 $git\n185.199.110.154 github.githubassets.com\n185.199.110.133 camo.githubassets.com\n" >> /etc/hosts; 
-    /etc/init.d/dnsmasq restart 2>/dev/null; 
-}; 
-echo -e "Гитхаб разлочен"
+git="github.com"; grep -q "^140.82.114.3 $git" /etc/hosts || {
+    printf "#$git\n140.82.114.3 $git\n185.199.110.154 github.githubassets.com\n185.199.110.133 camo.githubassets.com\n" >> /etc/hosts
+    /etc/init.d/dnsmasq restart 2>/dev/null
+}
+printf "Гитхаб разлочен\n"
 
 # Скачивание и установка темы Aurora
-cd /tmp || exit 1
-uclient-fetch -O luci-theme-aurora.apk https://github.com/eamonxg/luci-theme-aurora/releases/latest/download/luci-theme-aurora-1.1.0-r20260711.apk || {
-    echo "Ошибка загрузки темы Aurora. Интернет есть?"
-    exit 1
-}
-apk add --allow-untrusted luci-theme-aurora.apk || {
-    echo "Ошибка установки темы Aurora"
+wget -O - https://openwrt.eamonxg.fun/install.sh | sh || {
+    echo "Ошибка загрузки темы Aurora. Скачаешь вручную после настройки NetShift"
     exit 1
 }
 
-# Установка sing-box-extended
-uclient-fetch -O sing-box-extended.apk https://github.com/shtorm-7/sing-box-extended/releases/download/v1.13.14-extended-2.5.2/sing-box-extended_1.13.14-extended-2.5.2_openwrt_aarch64_cortex-a53.apk || {
-    echo "Ошибка загрузки sing-box-extended. Есть 22 мегабайта?"
+# Установка sing-box-extended (последний релиз под архитектуру)
+ARCH="aarch64_cortex-a53"
+APK_URL=$(uclient-fetch -qO- "https://api.github.com/repos/shtorm-7/sing-box-extended/releases/latest" \
+  | grep -o '"browser_download_url": *"[^"]*'"$ARCH"'[^"]*\.apk"' \
+  | head -n1 | sed 's/.*"\(https[^"]*\)".*/\1/')
+
+[ -z "$APK_URL" ] && {
+    echo "APK под $ARCH не найден в последнем релизе"
     exit 1
 }
-apk add --allow-untrusted sing-box-extended.apk || {
-    echo "Ошибка установки sing-box-extended"
+
+echo "Качаю: $APK_URL"
+cd /tmp || exit 1
+uclient-fetch -O 1.apk "$APK_URL" || {
+    echo "Ошибка загрузки signbox-extended"
+    exit 1
+}
+apk add --allow-untrusted 1.apk || {
+    echo "Ошибка установки signbox-extended"
     exit 1
 }
 
@@ -36,9 +44,7 @@ apk add --allow-untrusted sing-box-extended.apk || {
 rm -f /tmp/*.apk
 
 # Установка netshift (основной скрипт)
-sh <(wget -O - https://raw.githubusercontent.com/yandexru45/netshift/refs/heads/main/install.sh) || {
+wget -O - https://raw.githubusercontent.com/yandexru45/netshift/refs/heads/main/install.sh | sh || {
     echo "Ошибка установки netshift"
     exit 1
 }
-
-echo "Все компоненты успешно установлены!"
